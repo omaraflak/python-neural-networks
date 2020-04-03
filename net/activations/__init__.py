996d3056afc05d632b27b3f18b32b6b0b536b544
Omar Aflak <aflakomar@gmail.com>
2020-04-03 14:45:40 +0200

    cleaned

--- net/activations/__init__.py
+++ net/activations/__init__.py
@@ -1,3 +1,4 @@
 from .sigmoid import sigmoid, sigmoid_prime
 from .tanh import tanh, tanh_prime
 from .relu import relu, relu_prime
+from .parametric_relu import parametric_relu, parametric_relu_prime
