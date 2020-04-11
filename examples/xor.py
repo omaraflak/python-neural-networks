3406abb431f1654cf096c4ced6b4dcbd7fb0a685
Omar Aflak <aflakomar@gmail.com>
2020-04-11 16:02:32 +0200

    fixed xor

--- examples/xor.py
+++ examples/xor.py
@@ -3,46 +3,43 @@ sys.path.append('..')
 
 import numpy as np
 
-from net.layers import FCLayer, SoftmaxLayer, ActivationLayer
+from net.layers import Dense, Activation
 from net.activations import tanh, tanh_prime
 from net.losses import mse, mse_prime
-from net.utils import forward, backward
+from net.utils import create_model, forward, backward, update
+from net.optimizers import SGD
 
-X = [[0, 0], [0, 1], [1, 0], [1, 1]]
-Y = [[0], [1], [1], [0]]
+X = np.reshape([[0, 0], [0, 1], [1, 0], [1, 1]], (4, 1, 2))
+Y = np.reshape([[0], [1], [1], [0]], (4, 1, 1))
 
-X = np.reshape(X, (4, 1, 2))
-Y = np.reshape(Y, (4, 1, 1))
-
-network = [
-    FCLayer(2, 3),
-    ActivationLayer(tanh, tanh_prime),
-    FCLayer(3, 1),
-    ActivationLayer(tanh, tanh_prime)
-]
+model = create_model([
+    Dense(2, 3),
+    Activation(tanh, tanh_prime),
+    Dense(3, 1),
+    Activation(tanh, tanh_prime)
+], SGD, {'learning_rate': 0.1})
 
 epochs = 1000
-learning_rate = 0.1
 
 # training
 for epoch in range(epochs):
     error = 0
     for x, y in zip(X, Y):
         # forward
-        output = forward(network, x)
+        output = forward(model, x)
 
         # error (display purpose only)
         error += mse(y, output)
 
         # backward
-        backward(network, mse_prime(y, output))
+        backward(model, mse_prime(y, output))
 
         # update parameters
-        for layer in network:
-            layer.update(learning_rate)
+        update(model)
 
     error /= len(X)
     print('%d/%d, error=%f' % (epoch + 1, epochs, error))
 
 # test
-print(forward(network, X))
+for x in X:
+    print(forward(model, x))
