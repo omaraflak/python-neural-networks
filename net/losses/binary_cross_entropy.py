2b840ff055133510464408a8bef5d50e02438bd0
Omar Aflak <aflakomar@gmail.com>
2020-04-12 13:25:40 +0200

    used sigmoid in bce

--- net/losses/binary_cross_entropy.py
+++ net/losses/binary_cross_entropy.py
@@ -1,25 +1,20 @@
 import numpy as np
 from net.losses.loss import Loss
-
-def sigmoid(x):
-    return 1. / (1. + np.exp(-x))
-
-def sigmoid_prime(x):
-    s = sigmoid(x)
-    return s * (1 - s)
+from net.losses.activations import Sigmoid
 
 class BinaryCrossEntropy(Loss):
     def __init__(self, from_logits=False):
         self.from_logits = from_logits
+        self.s = Sigmoid()
 
     def call(self, y_true, y_pred):
         if self.from_logits:
-            return -np.log(sigmoid(y_pred)) if y_true == 1 else -np.log(1 - sigmoid(y_pred))
+            return -np.log(self.s.call(y_pred)) if y_true == 1 else -np.log(1 - self.s.call(y_pred))
         else:
             return -np.log(y_pred) if y_true == 1 else -np.log(1 - y_pred)
 
     def prime(self, y_true, y_pred):
         if self.from_logits:
-            return -sigmoid_prime(y_pred) / sigmoid(y_pred) if y_true == 1 else sigmoid_prime(y_pred) / (1 - sigmoid(y_pred))
+            return -self.s.prime(y_pred) / self.s.call(y_pred) if y_true == 1 else self.s.prime(y_pred) / (1 - self.s.call(y_pred))
         else:
             return -1 / y_pred if y_true == 1 else 1 / (1 - y_pred)
