e4d986f1d34a54998b018e8622498fbd112b36c0
Omar Aflak <aflakomar@gmail.com>
2020-10-30 10:40:25 +0100

    cleaned

--- net/activations/leaky_relu.py
+++ net/activations/leaky_relu.py
@@ -1,8 +1,12 @@
 import numpy as np
 from net.layers.activation import Activation
 
+def leaky_relu(x):
+    return ((x > 0) * x) + ((x <= 0) * p * x)
+
+def leaky_relu_prime(x):
+    return (x > 0) + ((x <= 0) * p)
+
 class LeakyReLU(Activation):
     def __init__(self, p=0.3, **kwargs):
-        leaky_relu = lambda x: ((x > 0) * x) + ((x <= 0) * p * x)
-        leaky_relu_prime = lambda x: (x > 0) + ((x <= 0) * p)
         super().__init__(leaky_relu, leaky_relu_prime, **kwargs)
