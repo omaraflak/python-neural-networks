11dfc3c6c6249b9529a9019d609dd42d18e8fe09
Omar Aflak <aflakomar@gmail.com>
2020-10-30 12:19:07 +0100

    normal mean and std

--- net/initializers/normal.py
+++ net/initializers/normal.py
@@ -2,5 +2,10 @@ import numpy as np
 from .initializer import Initializer
 
 class Normal(Initializer):
+    def __init__(self, mean=0, std=1):
+        super().__init__()
+        self.mean = mean
+        self.std = std
+
     def get(self, *shape):
-        return np.random.randn(*shape)
+        return np.random.normal(self.mean, self.std, shape)
